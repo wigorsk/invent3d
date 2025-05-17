0477f8030f5d831797759411ffcfcceca4da65cc
Wigor Skorek <wigorskorek@gmail.com>
2025-05-17 15:29:37 -0300

    Initial React project setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(
+    <>
+        <h1>Foi</h1>
+    </>
+)
